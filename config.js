3f60d947329213c8e02620aa9a164dc86b2181ef
joebobbio <profjstark2@gmail.com>
2023-04-17 13:26:48 -0400

    QoL fixes
    * Removed placeholder content and instead use input field placeholders
    
    * Make UI responsive and mobile-friendly

--- config.js
+++ config.js
@@ -1,17 +1,11 @@
 module.exports = {
-  "links": [
-    "www.onspotify.xyz",
-    "portal.itutoring.xyz",
-    "www.mathhog.xyz",
-    "accelerated.mathcalculatoradvanced.xyz",
-    "k12.studybetter.xyz"
-  ],
+  "links": [],
   "logging": true,
   "logToHook": true,
-  "reportWebhook": "https://discord.com/api/webhooks/1005574127916171344/5jj6_npmbnbgDjmlmKokzvMg42WlV0Py451fGu7zuTn3-j1uwGxGjIkZk5hqSyf8DVbM",
-  "logWebhook": "https://discord.com/api/webhooks/1005574127916171344/5jj6_npmbnbgDjmlmKokzvMg42WlV0Py451fGu7zuTn3-j1uwGxGjIkZk5hqSyf8DVbM",
-  "userLimit": "5",
-  "buttonEmoji": "🌌",
-  "buttonLabel": "Proxy",
-  "buttonStyle": "Primary"
+  "reportWebhook": "",
+  "logWebhook": "",
+  "userLimit": "",
+  "buttonEmoji": "",
+  "buttonLabel": "",
+  "buttonStyle": ""
 }
